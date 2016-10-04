4949a2e394c7b696cad00ac4002872f9ff46d08f
Andrew Breckenridge <asbreckenridge@me.com>
2016-10-03 21:39:59 -0700

    use guards instead of nested ifs

--- RxParseCallbacks/RxParseCallbacks.swift
+++ RxParseCallbacks/RxParseCallbacks.swift
@@ -1,6 +1,10 @@
 import Foundation
 import RxSwift
 
+public enum Error: Swift.Error {
+    case noObjectNorErrorIncluded
+}
+
 class ParseRxCallbacks {
     
     static func createWithCallback<T>(_ callback: @escaping ((AnyObserver<T>) -> Void)) -> Observable<T> {
@@ -12,34 +16,39 @@ class ParseRxCallbacks {
     
     static func rx_parseCallback<T>(_ observer: AnyObserver<T>) -> (_ object: T, _ error: Error?) -> Void {
         return { (object: T, error: Error?) in
-            if error == nil {
-                observer.on(.next(object))
-                observer.on(.completed)
-            } else {
+            guard error == nil else {
                 observer.on(.error(error!))
             }
+            
+            observer.on(.next(object))
+            observer.on(.completed)
         }
     }
     
     static func rx_parseUnwrappedOptionalCallback<T>(_ observer: AnyObserver<T>) -> (_ object: T?, _ error: Error?) -> Void {
         return { (object: T?, error: Error?) in
-            if error == nil {
-                observer.on(.next(object!))
-                observer.on(.completed)
-            } else {
+            guard error == nil else {
                 observer.on(.error(error!))
+            }
+            
+            switch object {
+            case .some(let obj):
+                observer.on(.next(obj))
+                observer.on(.completed)
+            case .none:
+                observer.on(.error(Error.noObjectNorErrorIncluded))
             }
         }
     }
     
     static func rx_parseOptionalCallback<T>(_ observer: AnyObserver<T?>) -> (_ object: T?, _ error: Error?) -> Void {
         return { (object: T?, error: Error?) in
-            if error == nil {
-                observer.on(.next(object))
-                observer.on(.completed)
-            } else {
+            guard error == nil else {
                 observer.on(.error(error!))
             }
+            
+            observer.on(.next(object))
+            observer.on(.completed)
         }
     }
     
